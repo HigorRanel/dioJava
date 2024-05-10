30b9d13fdd05ee5a679d5d60111ccf6cc13af745
HigorRanel <higorranel@gmail.com>
2024-05-10 09:52:15 -0300

    teste

---
+++ anatomiaDeClasses/src/MinhaClasse.java
@@ -0,0 +1,22 @@
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.println("Hello Word!");
+        String primeiroNome = "Higor";
+        String segundoNome = "Ranel";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println( nomeCompleto);
+
+
+      //  final String BR = "Brasil"; //não pode ser mudada *constante"
+        
+        
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
+
+}
+
